ca5ea422d9b8274dbfd7d6e14bfc3689601ee3c5
Brandon <leonardo.blomm@gmail.com>
2025-10-20 17:25:08 -0600

    feat(gifApp): agregue primeros pasos para visualizacion de comoponentes

---
+++ src/GifsApp.tsx
@@ -0,0 +1,46 @@
+import { mockGifs } from "./mock-data/gifs.mocks";
+import { SearchBar } from "./shared/components/SearchBar";
+import { MockGifs } from "./gifs/components/GifsList";
+import { CustomHeader } from "./shared/components/CustomHeader";
+import { PreviousSearches } from "./gifs/components/PreviousSearches";
+import { useState } from "react";
+
+export const GifsApp = () => {
+  const [previousTerms, setPreviousTerms] = useState(["dragon ball z"]);
+
+  const handleTermClicked = (term: string) => {
+    console.log(term);
+  };
+
+  const handleSerach = (query: string = '') => {
+    query = query.trim().toLowerCase();
+
+    if (query.length === 0) return;
+
+    if (previousTerms.includes(query)) return;
+
+    setPreviousTerms([query, ...previousTerms].splice(0, 8));
+  };
+
+  return (
+    <>
+      {/* Header */}
+      <CustomHeader
+        title="Buscador de Gifs"
+        description="Descrube y comparte el Gif perfecto"
+      />
+
+      {/* Search */}
+      <SearchBar placeholder="Busca lo que quieras" onQuery={handleSerach} />
+
+      {/* Busquedas previas */}
+      <PreviousSearches
+        searches={previousTerms}
+        onLabelClicked={handleTermClicked}
+      />
+
+      {/* Gifs */}
+      <MockGifs gifs={mockGifs} />
+    </>
+  );
+};
---
+++ src/shared/components/SearchBar.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState, type KeyboardEvent } from "react";
+
+interface Props {
+  placeholder?: string;
+  onQuery: (query: string) => void;
+}
+export const SearchBar = ({ placeholder = "Buscar", onQuery }: Props) => {
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      onQuery(query);
+    }, 700);
+    return () => {
+      clearTimeout(timeOutId);
+    };
+  }, [query, onQuery]);
+
+  const handelSearch = () => {
+    onQuery(query);
+    //setQuery('')
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handelSearch();
+    }
+  };
+
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handelSearch}>Buscar</button>
+    </div>
+  );
+};
